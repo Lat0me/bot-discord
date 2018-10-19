05a0d79c57fecbd98ac0d4ea693bd96bfbc778a7
Antoine Cordier <antoine27cordier@gmail.com>
2018-10-19 12:56:46 +0000

    Transférer les fichiers vers ''

---
+++ index.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const Command = require('./command/command.js');
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', message => {
+    if (!message.guild) return;
+
+    Command.Ping(message);
+    Command.moderation(message);
+
+});
+
+
+client.login('NTAyNzc5MjYxNDg5NTEyNDQ4.Dqs68A.WethyzkdwOTRZLJBlvw6WNtiVa0');
